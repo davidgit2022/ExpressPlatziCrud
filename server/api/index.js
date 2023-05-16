49a9102ecda5a95036f91a6e153d8ac73a162a2e
Deivy Potosi <deibypotosi535@gmail.com>
2023-05-16 15:58:27 -0500

    Preparado para subir a vercel2

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,8 @@
 import app from "./app.js";
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen( port,() => {
-  console.log(`server running on port ${port}`);
+app.listen( PORT,() => {
+  console.log(`server running on port ${PORT}`);
   console.log('http://localhost:3000/api');
 })
